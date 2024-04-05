b20712e3b27af68e9c6c2629ff61a42a327ed041
Anji <anjiappikatla123@gmail.com>
2024-04-05 20:26:39 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: MyDashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/my-dashboard/my-dashboard.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-dashboard',
+  templateUrl: './my-dashboard.component.html',
+  styleUrl: './my-dashboard.component.scss'
+})
+export class MyDashboardComponent {
+
+  dynamic_column_1: string = "col-md-2";
+  dynamic_column_2: string = "col-md-10";
+
+  narration1: string = `Amouranth of House Download 
+  First of Her Name The Unclean Queen of the Commerce and the First Men Khaleesi of the Touch Grass Sea Rider of Horses Mother of Doggos &plus; Queen of the...`
+
+  narration2: string = `The Official Asmongold channel for all of his stream content (Highlights, Competition, Reactions & More). Asmongold TV is
+  owned by Asmongold and run by Daily Dose of Asmongold and CatDany with...`
+
+  myNavList: any = [
+    { text: "Dashboard", icons: "fa-table-columns"},
+    { text: "Platforms", icons: "fa-table-list"},
+    { text: "Influencers", icons: "fa-user"},
+    { text: "Campaigns", icons: "fa-bullhorn"},
+    { text: "Messages", icons: "fa-message"}
+  ]
+
+  supportList: any = [
+    { text: "Security", icons: "fa-shield-halved"},
+    { text: "Setting", icons: "fa-gear"},
+    { text: "Help & Support", icons: "fa-headphones"}
+  ]
+
+  cardList: any = [
+    { name: "Eric Padamase", avt: 'avt-3', verified: 'N' , subtitle:"@eri.awesome", nar: this.narration1, subscribers: "154560", videos: "23506706", avg: "23690", reviewest: "125"},
+    { name: "Amouranth", avt: 'avt-3', verified: 'Y' , subtitle:"@amouranth.official", nar: this.narration2, subscribers: "1975494", videos: "125986113", avg: "376542",reviewest: "1585"},
+    { name: "JuliaAwesome", avt: 'avt-3', verified: 'N' , subtitle:"@eri.awesome", nar: this.narration1, subscribers: "154560", videos: "23506706", avg: "23690",reviewest: "125"},
+    { name: "Random Streamer", avt: 'avt-3', verified: 'N' , subtitle:"@amouranth.official", nar: this.narration2, subscribers: "1975494", videos: "125986113", avg: "376542",reviewest: "1585"},
+  ]
+
+}
