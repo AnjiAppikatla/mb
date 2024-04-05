627ae5290a849a30b3c7290c3653b489d9c17322
Anji <anjiappikatla123@gmail.com>
2024-04-05 23:31:05 +0530

    changes-2

--- src/app/my-dashboard/my-dashboard.component.ts
+++ src/app/my-dashboard/my-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-my-dashboard',
@@ -9,6 +10,8 @@ export class MyDashboardComponent {
 
   dynamic_column_1: string = "col-md-2";
   dynamic_column_2: string = "col-md-10";
+  iconcolumn: string = 'col-md-2'
+  text_columns: boolean = true;
 
   narration1: string = `Amouranth of House Download 
   First of Her Name The Unclean Queen of the Commerce and the First Men Khaleesi of the Touch Grass Sea Rider of Horses Mother of Doggos &plus; Queen of the...`
@@ -36,5 +39,20 @@ export class MyDashboardComponent {
     { name: "JuliaAwesome", avt: 'avt-3', verified: 'N' , subtitle:"@eri.awesome", nar: this.narration1, subscribers: "154560", videos: "23506706", avg: "23690",reviewest: "125"},
     { name: "Random Streamer", avt: 'avt-3', verified: 'N' , subtitle:"@amouranth.official", nar: this.narration2, subscribers: "1975494", videos: "125986113", avg: "376542",reviewest: "1585"},
   ]
+
+  toggleClick(){
+    if(this.dynamic_column_1 == 'col-md-2'){
+      this.dynamic_column_1 = 'col-md-1';
+      this.dynamic_column_2 = 'col-md-11'
+      this.iconcolumn = 'col-md-12 text-center f-25 text-white'
+      this.text_columns = false;
+    }
+    else{
+      this.dynamic_column_1 = 'col-md-2';
+      this.dynamic_column_2 = 'col-md-10'
+      this.iconcolumn = 'col-md-2'
+      this.text_columns = true;
+    }
+  }
 
 }
